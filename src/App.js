99462087b1dae36e5df0717382676ffea1dd0996
hind-wihaidi <hind.wihaidi05~gmail.com>
2021-11-14 16:51:23 +0200

    Uploaded the homework

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React, {Component} from 'react'
-import Add from './Add'
+import React, {useState} from 'react'
 
 function App() {
+  const [count, setCount]=useState(0)
+  function Add(){
+    setCount(count+1)
+  }
+  function Subtract(){
+    setCount(count-1)
+  }
+  function Clear(){
+    setCount(0)
+  }
   return (
     <div>
-      <Add/>
+      <h1>{count}</h1>
+      <button onClick={Add}>Plus</button>
+      <button onClick={Subtract}>Minus</button>
+      <button onClick={Clear}>Clear</button>
     </div>
   );
 }
